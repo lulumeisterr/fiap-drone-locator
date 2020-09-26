0a3467513cc6cc7c900fc12f470f7222ee68a016
Lucas Rodrigues <lucasrodriguesdonascimento@MacBook-Pro.local>
2020-09-26 14:30:01 -0300

    implementando nova thread para validacao de temperatura

--- consumer/src/main/java/br/com/fiap/consumer/service/implementation/ConsumerServiceImpl.java
+++ consumer/src/main/java/br/com/fiap/consumer/service/implementation/ConsumerServiceImpl.java
@@ -4,6 +4,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import br.com.fiap.consumer.dto.Message;
 import br.com.fiap.consumer.sendEmail.Email;
@@ -18,7 +21,7 @@ Para enviar e-mail pode usar um SaaS.
  */
 @Service
 public class ConsumerServiceImpl implements ConsumerService{
-	
+
 	@Autowired
 	private Email email;
 
@@ -26,23 +29,39 @@ public class ConsumerServiceImpl implements ConsumerService{
 	public void action(Message message) throws Exception {
 		System.out.println("Temperatura : "+ message.getText().getTemperatura() + 
 				" E " + " Umidade : " + message.getText().getUmidade() + message.getText().getIdDrone());
-		
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+
+				try {
+					regra(message);
+				} catch (Exception e ) {
+					e.printStackTrace();
+				}
+
+			}
+		}).start();
+
+	}
+
+	private void regra(Message message) throws InterruptedException {
 		if(message.getText().getTemperatura() >= 35 || message.getText().getTemperatura() <= 0 
 				|| message.getText().getUmidade() <= 15){
-			
+
 			StringBuilder msgEmail = new StringBuilder()
 					.append(" ID DO DRONE : " + message.getText().getIdDrone()).append("\n")
 					.append(" Temperatura : " + message.getText().getTemperatura()).append("\n")
 					.append(" Umidade : " + message.getText().getUmidade());
-			
-			
+
 			System.out.println("Aguardando 1m para encaminhar o email");
 			TimeUnit.MINUTES.sleep(1);
-			
+
 			System.out.println("Encaminhando Email");
 			email.sendEmail(msgEmail.toString());
+		}else {
+			System.out.println("Sem Emails para encaminhar");
 		}
-		
 	}
 
 }
